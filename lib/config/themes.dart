74e730994d16c82cb2145a97fcaee551df1c0670
arthur Windows <rybalkin.arthur@gmail.com>
2024-10-28 00:05:20 +0100

    theme

--- lib/config/themes.dart
+++ lib/config/themes.dart
@@ -2,17 +2,20 @@ import 'package:flutter/material.dart';
 
 Color ckBackgroundColor = Color(0xFF1ABC9C);
 Color DarkThemeTextColorAccent = Color(0xFFBFBFBF);
+Color DarkThemeTextColor = Colors.white;
 
 ThemeData darkTheme = ThemeData(
     brightness: Brightness.dark,
     useMaterial3: true,
     textTheme: TextTheme(
-// displayLarge: TextStyle(
-// fontSize: 32, fontWeight: FontWeight.bold, color: lightThemeTextColor),
-// headlineMedium: TextStyle(
-// fontSize: 24, color: lightThemeTextColor), // Keep the headlineMedium
-// bodyLarge: TextStyle(fontSize: 18, color: lightThemeTextColor),
-// bodyMedium: TextStyle(fontSize: 16, color: lightThemeTextColor),
+        displayLarge: TextStyle(
+            fontSize: 32,
+            //fontWeight: FontWeight.bold,
+            color: DarkThemeTextColor),
+        headlineMedium: TextStyle(
+            fontSize: 24, color: DarkThemeTextColor), // Keep the headlineMedium
+        bodyLarge: TextStyle(fontSize: 18, color: DarkThemeTextColor),
+        bodyMedium: TextStyle(fontSize: 16, color: DarkThemeTextColor),
         bodySmall: TextStyle(fontSize: 14, color: DarkThemeTextColorAccent))
     //     ),
     // appBarTheme: const AppBarTheme(
--- lib/features/overview/overview_screen.dart
+++ lib/features/overview/overview_screen.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:ts_4_8_1_eigene_app_ui/features/blog/data/blog_data.dart';
 import 'package:ts_4_8_1_eigene_app_ui/features/blog/widgets/blogpost_widget.dart';
+import 'package:ts_4_8_1_eigene_app_ui/features/calendar/data/calendar_data.dart';
+import 'package:ts_4_8_1_eigene_app_ui/features/calendar/widgets/calendar_widget.dart';
 
 class OverviewScreen extends StatefulWidget {
-  const OverviewScreen({super.key, required this.blogData});
+  const OverviewScreen({
+    super.key,
+    required this.blogData,
+  });
   final BlogData blogData;
+  //final CalendarData calendarData;
 
   @override
   State<OverviewScreen> createState() => _OverviewScreenState();
@@ -21,6 +27,7 @@ class _OverviewScreenState extends State<OverviewScreen> {
       child: Column(
         children: [
           BlogpostWidget(myBlogpost: widget.blogData.getRandom()),
+          //CalendarWidget()
         ],
       ),
     );
