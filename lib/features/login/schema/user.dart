f51c3067df548d0383571de642f9b60ccf910c3a
arthur Windows <rybalkin.arthur@gmail.com>
2024-12-05 13:02:42 +0100

    added gif

--- lib/features/login/schema/user.dart
+++ lib/features/login/schema/user.dart
@@ -4,10 +4,12 @@ class User {
   final String name;
   final bool isAdmin;
   final bool isGuest;
+  final String? userAvatar;
   User(
       {required this.email,
       required this.password,
       required this.name,
       this.isAdmin = false,
-      this.isGuest = false});
+      this.isGuest = false,
+      this.userAvatar});
 }
--- lib/features/profile/profile_screen.dart
+++ lib/features/profile/profile_screen.dart
@@ -15,8 +15,10 @@ class _ProfileScreenState extends State<ProfileScreen> {
         const Text("Spielwiese"),
         SizedBox(
             width: MediaQuery.sizeOf(context).width,
-            height: 600,
+            //height: 600,
             child: const Text("UserSettings")),
+        Image.network(
+            'https://no-cdn.shortpixel.ai/client/to_avif,q_lossy,ret_wait/https://shortpixel.com/blog/wp-content/uploads/2023/12/nyan-cat.gif')
       ],
     );
   }
